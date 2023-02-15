6a6b9878be9b776b75f3302ef5b9eb98fb6c2aa7
MoeQatoum <mhmd.qatoum@hotmail.com>
2023-02-15 06:17:58 +0400

    QT_IMPLEMENTATION for log_sorted_rows

--- include/df_algo.h
+++ include/df_algo.h
@@ -117,21 +117,55 @@ namespace df {
 
   template<typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
   void log_sorted_rows(const List<RowSeries<T>>& sorted_rows, DataFrame<T>& df, long range = 0) {
-#ifdef QT_IMPLEMENTATION
-#else
+
+    DF_ASSERT(tail > m_row_count && tail >= 1, "tail is grater then row count");
+
     sizetype spacing   = 5;
     sizetype idx_space = 4;
 
     sizetype row_name_space = df.max_row_name_size() + spacing;
     sizetype col_spacing    = df.max_col_name_size() + spacing;
 
+#ifdef QT_IMPLEMENTATION
+    QDebug dbg = clog.noquote().nospace();
+
+    dbg << String("%1").arg("idx", -(df.max_row_name_size() + spacing + idx_space));
+    for (auto& c : sorted_rows[0]) {
+      dbg << String("%1").arg(c->idx.col_name, -(df.max_col_name_size() + spacing));
+    }
+    dbg << "\n";
+
+    sizetype range_start;
+    sizetype range_end;
+    if (range == 0) {
+      range_start = 0;
+      range_end   = sorted_rows.size();
+    } else if (range > 0) {
+      range_start = 0;
+      range_end   = static_cast<sizetype>(range);
+    } else {
+      range_start = sorted_rows.size() + static_cast<sizetype>(range);
+      range_end   = sorted_rows.size();
+    }
+
+    for (sizetype idx = range_start; idx < range_end; idx++) {
+      const RowSeries<T>& row = sorted_rows[idx];
+      dbg << String("%1").arg(row.idx(), -idx_space) << String("%1").arg(row.name(), -(row_name_space));
+      for (const auto& c : row) {
+        if (std::is_floating_point_v<T>) {
+          dbg << String("%1").arg(String::number(c->value, 'f', df.floatPrecision()), -10);
+        } else {
+          dbg << String("%1").arg(c->value, -(col_spacing));
+        }
+      }
+      dbg << "\n";
+    }
+#else
     clog << std::left << std::setw((df.max_row_name_size() + spacing + idx_space)) << "idx";
     for (auto& c : sorted_rows[0]) {
       clog << std::left << std::setw(df.max_col_name_size() + spacing) << c->idx.col_name;
     }
     clog << "\n";
-
-    DF_ASSERT(tail > m_row_count && tail >= 1, "tail is grater then row count");
 
     sizetype range_start;
     sizetype range_end;
