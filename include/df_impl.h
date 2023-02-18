0b3f10f74f78fedba5d9dd616baf67c4b9dafc90
MoeQatoum <mhmd.qatoum@hotmail.com>
2023-02-18 19:09:37 +0400

    better assertion

--- include/df_impl.h
+++ include/df_impl.h
@@ -443,10 +443,8 @@ public:
     }
 
     const ValueType& operator[](const sizetype& col_idx, const sizetype& row_idx) const {
-      static_assert(col_idx > row_size(), "col index out of range");
-
-      // DF_ASSERT(0 <= col_idx && col_idx <= (m_col_size - 1), "index out of range");
-      // DF_ASSERT(0 <= row_idx && row_idx <= (m_row_size - 1), "index out of range");
+      DF_ASSERT(0 <= col_idx && col_idx <= (m_col_size - 1), "index out of range");
+      DF_ASSERT(0 <= row_idx && row_idx <= (m_row_size - 1), "index out of range");
 
       return *(m_d + ((m_row_size * row_idx) + col_idx));
     }
