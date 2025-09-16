efc28b61842f52567853c458394c6061d61ed55b
MoeQatoum <mhmd.qatoum@hotmail.com>
2025-09-17 02:31:30 +0400

    check linker flags

--- cmake/compilerFlags.cmake
+++ cmake/compilerFlags.cmake
@@ -3,7 +3,7 @@ set(DF_TCMALLOC_FLAGS -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-reall
 set(DF_TCMALLOC_LINKER_FLAGS -ltcmalloc_minimal)
 
 set(DF_STD_LIB_CXX_FLAGS -stdlib=libc++)
-set(DF_STD_LIB_CXX_LINKER_FLAGS -stdlib=libc++) # -nostdlib++)
+set(DF_STD_LIB_CXX_LINKER_FLAGS -lc++ -nostdlib++) # or -stdlib=libc++)
 
 set(DF_REL_BUILD_FLAGS -O3 -DNDEBUG -s)
 set(DF_REL_WARN_FLAGS -Wall -Wextra -Wreorder-ctor -Wpedantic -Wdouble-promotion)
